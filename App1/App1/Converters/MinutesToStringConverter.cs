645dbbbe3bf2822140fd1e2738d070564c8c801d
Will Beal <willbeal@outlook.com>
2021-05-04 16:19:20 +0200

    Add project files.

---
+++ App1/App1/Converters/MinutesToStringConverter.cs
@@ -0,0 +1,32 @@
+﻿using NodaTime;
+using NodaTime.Text;
+using System;
+using System.Globalization;
+using Xamarin.Forms;
+
+namespace App1.Converters
+{
+    public class MinutesToStringConverter : IValueConverter
+    {
+        public object? Convert(object value, Type targetType, object parameter, CultureInfo culture) => value is int minutes
+            ? Duration.FromMinutes(minutes).ToString("H:mm", CultureInfo.InvariantCulture)
+            : null;
+
+        public object? ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if (value is string timeString)
+            {
+                var result = DurationPattern.CreateWithInvariantCulture("H:mm").Parse(timeString);
+                if (result.Success)
+                {
+                    return (int)result.Value.TotalMinutes;
+                }
+                else
+                {
+                    return null;
+                }
+            }
+            else return null;
+        }
+    }
+}
---
+++ App1/App1/MainPageViewModel.cs
@@ -0,0 +1,25 @@
+﻿using MvvmHelpers;
+using System.Windows.Input;
+using Xamarin.Forms;
+
+namespace App1
+{
+    public class MainPageViewModel : BaseViewModel
+    {
+        private int? timeMinutes = null;
+        public int? TimeMinutes
+        {
+            get => timeMinutes;
+            set => SetProperty(ref timeMinutes, value);
+        }
+
+        public MainPageViewModel() => AutoCalculateCommand = new Command(AutoCalculate);
+
+        public ICommand AutoCalculateCommand { get; }
+
+        private void AutoCalculate()
+        {
+            TimeMinutes = 90;
+        }
+    }
+}
